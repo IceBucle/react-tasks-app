f02ce70c640ed5b591cff71eeb978c47226c4256
IceBucle <eloiespel@gmail.com>
2023-04-09 15:34:00 +0200

    first commit

---
+++ src/data/tasks.js
@@ -0,0 +1,17 @@
+export const tasks = [
+  {
+    id: 0,
+    title: "Mi primera tarea",
+    description: "Mi primera descripcion",
+  },
+  {
+    id: 1,
+    title: "Mi segunda tarea",
+    description: "Mi segunda descripcion",
+  },
+  {
+    id: 2,
+    title: "Mi tercera tarea",
+    description: "Mi tercera descripcion",
+  },
+];
